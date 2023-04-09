96575b5f1a8330540b50c6a50628ac03c9242b8b
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-09 16:24:39 +0530

    Initial commit

---
+++ src/main/java/dev/lokeshbisht/catalogserviceecommerce/CatalogServiceEcommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.catalogserviceecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lokeshbisht/catalogserviceecommerce/CatalogServiceEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.catalogserviceecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
