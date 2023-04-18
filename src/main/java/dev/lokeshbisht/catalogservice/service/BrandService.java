3bc86547eeea503cd15e347b2418ae8031882ca9
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-19 00:14:59 +0530

    feat(api): #22 Add api to fetch brand by brandId

--- src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
+++ src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
@@ -3,7 +3,10 @@ package dev.lokeshbisht.catalogservice.service;
 import dev.lokeshbisht.catalogservice.dto.brand.BrandDto;
 import dev.lokeshbisht.catalogservice.entity.Brand;
 
+import java.util.Optional;
+
 public interface BrandService {
 
   Brand createBrand(BrandDto brandDto);
+  Optional<Brand> getBrand(String brandId);
 }
