f3fc80a74fdc36e42eda92e03ebb1e779ed7e5ff
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-21 23:37:54 +0530

    feat(api): #33 Add api to fetch all brands by a category

--- src/main/java/dev/lokeshbisht/catalogservice/repository/BrandRepository.java
+++ src/main/java/dev/lokeshbisht/catalogservice/repository/BrandRepository.java
@@ -4,6 +4,7 @@ import dev.lokeshbisht.catalogservice.entity.Brand;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,5 @@ public interface BrandRepository extends MongoRepository<Brand, String> {
 
   Optional<Brand> findByBrandId(Integer brandId);
   void deleteByBrandId(Integer brandId);
+  List<Brand> findAllByCategoryId(Integer categoryId);
 }
--- src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
+++ src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
@@ -3,6 +3,7 @@ package dev.lokeshbisht.catalogservice.service;
 import dev.lokeshbisht.catalogservice.dto.brand.BrandDto;
 import dev.lokeshbisht.catalogservice.entity.Brand;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BrandService {
@@ -11,4 +12,5 @@ public interface BrandService {
   Optional<Brand> getBrand(String brandId);
   Brand updateBrand(String brandId, BrandDto brandDto);
   void deleteBrand(String brandId);
+  List<Brand> getAllBrandsByCategoryId(String categoryId);
 }
