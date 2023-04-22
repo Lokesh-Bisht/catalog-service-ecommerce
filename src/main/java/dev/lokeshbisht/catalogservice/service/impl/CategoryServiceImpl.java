4597792124436fb996e1c60f850d75c36ab4b57a
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-22 23:29:31 +0530

    feat(api): #36 Add api to create a category

---
+++ src/main/java/dev/lokeshbisht/catalogservice/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package dev.lokeshbisht.catalogservice.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.lokeshbisht.catalogservice.dto.category.CategoryDto;
+import dev.lokeshbisht.catalogservice.entity.Category;
+import dev.lokeshbisht.catalogservice.exceptions.CategoryAlreadyExistsException;
+import dev.lokeshbisht.catalogservice.exceptions.JsonRuntimeException;
+import dev.lokeshbisht.catalogservice.repository.CategoryRepository;
+import dev.lokeshbisht.catalogservice.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
+  @Override
+  public Category createCategory(CategoryDto categoryDto) {
+    logger.info("Saving category: {}", categoryDto);
+    if (!categoryRepository.findByCategoryId(categoryDto.getCategoryId()).isEmpty()) {
+      logger.error("Error occurred while saving category. Category already exists!");
+      throw new CategoryAlreadyExistsException("Category Already Exists.");
+    }
+    try {
+      Category category = objectMapper.readValue(objectMapper.writeValueAsString(categoryDto), Category.class);
+      category.setCreatedAt(Instant.now().getEpochSecond());
+      logger.info("Successfully added category: {}", category);
+      return categoryRepository.save(category);
+    } catch (JsonMappingException e) {
+      logger.error("Error occurred during saving document: {}", categoryDto);
+      throw new JsonRuntimeException("Json Mapping exception encountered during object to string conversion", e);
+    } catch (JsonProcessingException e) {
+      logger.error("Error occurred during saving document: {}", categoryDto);
+      throw new JsonRuntimeException("Json Processing exception encountered during object to string conversion.", e);
+    }
+  }
+}
