d7599261c8b23e7e46c51f55abf5f8bc96cd5de6
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-24 16:20:18 +0530

    feat(api): #39 Add api to get category by categoryId

--- src/main/java/dev/lokeshbisht/catalogservice/controller/CategoryController.java
+++ src/main/java/dev/lokeshbisht/catalogservice/controller/CategoryController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/category")
@@ -26,5 +25,11 @@ public class CategoryController {
   @PostMapping()
   Category createCategory(@Valid @RequestBody CategoryDto categoryDto) {
     return categoryService.createCategory(categoryDto);
+  }
+
+  @Operation(summary = "getCategory")
+  @GetMapping("/{id}")
+  Optional<Category> getCategory(@PathVariable("id") String categoryId) {
+    return categoryService.getCategory(categoryId);
   }
 }
--- src/main/java/dev/lokeshbisht/catalogservice/service/CategoryService.java
+++ src/main/java/dev/lokeshbisht/catalogservice/service/CategoryService.java
@@ -3,7 +3,10 @@ package dev.lokeshbisht.catalogservice.service;
 import dev.lokeshbisht.catalogservice.dto.category.CategoryDto;
 import dev.lokeshbisht.catalogservice.entity.Category;
 
+import java.util.Optional;
+
 public interface CategoryService {
 
   Category createCategory(CategoryDto categoryDto);
+  Optional<Category> getCategory(String categoryId);
 }
