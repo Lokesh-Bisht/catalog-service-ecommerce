90beb8ba5b37ee916ab83459345f6b9a42755a2c
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-29 23:51:54 +0530

    refactor: Move product dto to product package

--- src/main/java/dev/lokeshbisht/catalogservice/dto/product/ProductDto.java
+++ src/main/java/dev/lokeshbisht/catalogservice/dto/product/ProductDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 
 import java.util.Date;
 import java.util.List;
