0cae6a317247375409ca8d62387ed208ff9fc73f
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-29 23:53:39 +0530

    Add product search response and filter dto

---
+++ src/main/java/dev/lokeshbisht/catalogservice/dto/ProductSearchResponseDto.java
@@ -0,0 +1,29 @@
+package dev.lokeshbisht.catalogservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.lokeshbisht.catalogservice.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ProductSearchResponseDto {
+
+  @JsonProperty("products")
+  private List<Product> products;
+
+  @JsonProperty("page")
+  private Integer page;
+
+  @JsonProperty("size")
+  private Integer size;
+
+  @JsonProperty("count")
+  private Long count;
+}
