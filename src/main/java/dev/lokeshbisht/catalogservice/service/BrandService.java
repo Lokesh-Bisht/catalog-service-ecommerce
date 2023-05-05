193ca941962713df18d19fab4ab2ae34bfdf9aa1
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-05-05 23:58:43 +0530

    feat: #74

--- src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
+++ src/main/java/dev/lokeshbisht/catalogservice/service/BrandService.java
@@ -1,7 +1,10 @@
 package dev.lokeshbisht.catalogservice.service;
 
 import dev.lokeshbisht.catalogservice.dto.brand.BrandDto;
+import dev.lokeshbisht.catalogservice.dto.brand.BrandSearchFilterDto;
+import dev.lokeshbisht.catalogservice.dto.brand.BrandSearchResponseDto;
 import dev.lokeshbisht.catalogservice.entity.Brand;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +16,6 @@ public interface BrandService {
   Brand updateBrand(String brandId, BrandDto brandDto);
   void deleteBrand(String brandId);
   List<Brand> getAllBrandsByCategoryId(String categoryId);
+  BrandSearchResponseDto search(String query, Pageable page, BrandSearchFilterDto filter);
   void bulkCreateBrand(List<BrandDto> brandDtoList);
 }
