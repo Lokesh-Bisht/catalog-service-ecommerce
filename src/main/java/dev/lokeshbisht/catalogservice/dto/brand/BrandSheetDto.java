e9f229e4617d321d6350f67c599ac0885ec9ab90
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-07-31 23:52:05 +0530

    Add brandSheet dto

---
+++ src/main/java/dev/lokeshbisht/catalogservice/dto/brand/BrandSheetDto.java
@@ -0,0 +1,15 @@
+package dev.lokeshbisht.catalogservice.dto.brand;
+
+import lombok.Data;
+
+@Data
+public class BrandSheetDto {
+
+    private String brandId;
+
+    private String brandName;
+
+    private String categoryId;
+
+    private String description;
+}
