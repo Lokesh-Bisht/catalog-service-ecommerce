207162596db932644f07492233adf8eef5512dc0
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-09-07 23:51:56 +0530

    refactor: Correct formatting

--- src/main/java/dev/lokeshbisht/catalogservice/entity/Product.java
+++ src/main/java/dev/lokeshbisht/catalogservice/entity/Product.java
@@ -10,49 +10,49 @@ import java.util.List;
 @Data
 public class Product {
 
-  @Id
-  @JsonProperty("_id")
-  private String id;
+    @Id
+    @JsonProperty("_id")
+    private String id;
 
-  @JsonProperty("brand_id")
-  private Integer brandId;
+    @JsonProperty("brand_id")
+    private Integer brandId;
 
-  @JsonProperty("category_id")
-  private Integer categoryId;
+    @JsonProperty("category_id")
+    private Integer categoryId;
 
-  @JsonProperty("product_id")
-  @TextIndexed
-  private Integer productId;
+    @JsonProperty("product_id")
+    @TextIndexed
+    private Integer productId;
 
-  @JsonProperty("brand_name")
-  private String brandName;
+    @JsonProperty("brand_name")
+    private String brandName;
 
-  @JsonProperty("product_name")
-  @TextIndexed
-  private String productName;
+    @JsonProperty("product_name")
+    @TextIndexed
+    private String productName;
 
-  private String description;
+    private String description;
 
-  private List<String> photos;
+    private List<String> photos;
 
-  @JsonProperty("display_photo")
-  private String displayPhoto;
+    @JsonProperty("display_photo")
+    private String displayPhoto;
 
-  @JsonProperty("sold_count")
-  private Long soldCount;
+    @JsonProperty("sold_count")
+    private Long soldCount;
 
-  @JsonProperty("return_count")
-  private Long returnCount;
+    @JsonProperty("return_count")
+    private Long returnCount;
 
-  @JsonProperty("created_at")
-  private Long createdAt;
+    @JsonProperty("created_at")
+    private Long createdAt;
 
-  @JsonProperty("created_by")
-  private String createdBy;
+    @JsonProperty("created_by")
+    private String createdBy;
 
-  @JsonProperty("updated_at")
-  private Long updatedAt;
+    @JsonProperty("updated_at")
+    private Long updatedAt;
 
-  @JsonProperty("updated_by")
-  private String updatedBy;
+    @JsonProperty("updated_by")
+    private String updatedBy;
 }
