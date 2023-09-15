8644254b8c303bf862b8fb056ad657bffbb374c3
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-09-15 09:17:37 +0530

    reafactor: Correct code formatting

--- src/main/java/dev/lokeshbisht/catalogservice/service/impl/BrandServiceImpl.java
+++ src/main/java/dev/lokeshbisht/catalogservice/service/impl/BrandServiceImpl.java
@@ -30,108 +30,108 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class BrandServiceImpl implements BrandService {
 
-  @Autowired
-  private BrandRepository brandRepository;
+    @Autowired
+    private BrandRepository brandRepository;
 
-  @Autowired
-  private CustomBrandRepository customBrandRepository;
+    @Autowired
+    private CustomBrandRepository customBrandRepository;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-  @Autowired
-  private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-  @Value("${kafka.topic.brand}")
-  private String brandTopic;
+    @Value("${kafka.topic.brand}")
+    private String brandTopic;
 
-  private static final Logger logger = LoggerFactory.getLogger(BrandServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(BrandServiceImpl.class);
 
-  @Override
-  public Brand createBrand(BrandDto brandDto) {
-    logger.info("Saving brand: {}", brandDto);
-    if (!brandRepository.findByBrandId(brandDto.getBrandId()).isEmpty()) {
-      logger.error("Error occurred while saving brand. Brand already exists!");
-      throw new BrandAlreadyExistsException("Brand already exists!");
+    @Override
+    public Brand createBrand(BrandDto brandDto) {
+        logger.info("Saving brand: {}", brandDto);
+        if (!brandRepository.findByBrandId(brandDto.getBrandId()).isEmpty()) {
+            logger.error("Error occurred while saving brand. Brand already exists!");
+            throw new BrandAlreadyExistsException("Brand already exists!");
+        }
+        try {
+            Brand brand = objectMapper.readValue(objectMapper.writeValueAsString(brandDto), Brand.class);
+            brand.setCreatedAt(Instant.now().getEpochSecond());
+            logger.info("Successfully added brand: {}", brand);
+            return brandRepository.save(brand);
+        } catch (JsonMappingException e) {
+            logger.error("Error occurred during saving document: {}", brandDto);
+            throw new JsonRuntimeException("Json Mapping exception encountered during object to string conversion", e);
+        } catch (JsonProcessingException e) {
+            logger.error("Error occurred during saving document: {}", brandDto);
+            throw new JsonRuntimeException("Json Processing exception encountered during object to string conversion.", e);
+        }
     }
-    try {
-      Brand brand = objectMapper.readValue(objectMapper.writeValueAsString(brandDto), Brand.class);
-      brand.setCreatedAt(Instant.now().getEpochSecond());
-      logger.info("Successfully added brand: {}", brand);
-      return brandRepository.save(brand);
-    } catch (JsonMappingException e) {
-      logger.error("Error occurred during saving document: {}", brandDto);
-      throw new JsonRuntimeException("Json Mapping exception encountered during object to string conversion", e);
-    } catch (JsonProcessingException e) {
-      logger.error("Error occurred during saving document: {}", brandDto);
-      throw new JsonRuntimeException("Json Processing exception encountered during object to string conversion.", e);
+
+    @Override
+    public Optional<Brand> getBrand(String brandId) {
+        logger.info("Find brand with id: {}", brandId);
+        Optional<Brand> brand = brandRepository.findByBrandId(Integer.parseInt(brandId));
+        if (brand.isEmpty()) {
+            logger.error("Brand not found!");
+            throw new BrandNotFoundException("Brand not found!");
+        }
+        logger.info("Found brand: {}", brand);
+        return brand;
     }
-  }
 
-  @Override
-  public Optional<Brand> getBrand(String brandId) {
-    logger.info("Find brand with id: {}", brandId);
-    Optional<Brand> brand = brandRepository.findByBrandId(Integer.parseInt(brandId));
-    if (brand.isEmpty()) {
-      logger.error("Brand not found!");
-      throw new BrandNotFoundException("Brand not found!");
+    @Override
+    public Brand updateBrand(String brandId, BrandDto brandDto) {
+        logger.info("Update brand: {} with id: {}", brandDto, brandId);
+        Optional<Brand> brand = brandRepository.findByBrandId(Integer.parseInt(brandId));
+        if (brand.isEmpty()) {
+            logger.error("Brand with id {} is not found.", brandId);
+            throw new BrandNotFoundException("Brand not found!");
+        }
+        try {
+            Brand updatedBrand = objectMapper.readValue(objectMapper.writeValueAsString(brandDto), Brand.class);
+            updatedBrand.setId(brand.get().getId());
+            updatedBrand.setCreatedAt(brand.get().getCreatedAt());
+            updatedBrand.setCreatedBy(brand.get().getCreatedBy());
+            updatedBrand.setUpdatedAt(Instant.now().getEpochSecond());
+            logger.info("Successfully updated brand.");
+            return brandRepository.save(updatedBrand);
+        } catch (JsonMappingException e) {
+            logger.error("Error occurred during updating document: {}", brandDto);
+            throw new JsonRuntimeException("Json Mapping exception encountered during object to string conversion", e);
+        } catch (JsonProcessingException e) {
+            logger.error("Error occurred during updating document: {}", brandDto);
+            throw new JsonRuntimeException("Json Processing exception encountered during object to string conversion", e);
+        }
     }
-    logger.info("Found brand: {}", brand);
-    return brand;
-  }
 
-  @Override
-  public Brand updateBrand(String brandId, BrandDto brandDto) {
-    logger.info("Update brand: {} with id: {}", brandDto, brandId);
-    Optional<Brand> brand = brandRepository.findByBrandId(Integer.parseInt(brandId));
-    if (brand.isEmpty()) {
-      logger.error("Brand with id {} is not found.", brandId);
-      throw new BrandNotFoundException("Brand not found!");
+    @Override
+    public void deleteBrand(String brandId) {
+        logger.info("Delete brand with id: {}", brandId);
+        if (brandRepository.findByBrandId(Integer.parseInt(brandId)).isEmpty()) {
+            logger.error("Error deleting brand with id: {}", brandId);
+            throw new BrandNotFoundException("Brand not found!");
+        }
+        brandRepository.deleteByBrandId(Integer.parseInt(brandId));
+        logger.info("Brand with id {} is deleted successfully.", brandId);
     }
-    try {
-      Brand updatedBrand = objectMapper.readValue(objectMapper.writeValueAsString(brandDto), Brand.class);
-      updatedBrand.setId(brand.get().getId());
-      updatedBrand.setCreatedAt(brand.get().getCreatedAt());
-      updatedBrand.setCreatedBy(brand.get().getCreatedBy());
-      updatedBrand.setUpdatedAt(Instant.now().getEpochSecond());
-      logger.info("Successfully updated brand.");
-      return brandRepository.save(updatedBrand);
-    } catch (JsonMappingException e) {
-      logger.error("Error occurred during updating document: {}", brandDto);
-      throw new JsonRuntimeException("Json Mapping exception encountered during object to string conversion", e);
-    } catch (JsonProcessingException e) {
-      logger.error("Error occurred during updating document: {}", brandDto);
-      throw new JsonRuntimeException("Json Processing exception encountered during object to string conversion", e);
+
+    @Override
+    public List<Brand> getAllBrandsByCategoryId(String categoryId) {
+        logger.info("Get all brands with category id: {}", categoryId);
+        // Need to add category check
+        return brandRepository.findAllByCategoryId(Integer.parseInt(categoryId));
     }
-  }
 
-  @Override
-  public void deleteBrand(String brandId) {
-    logger.info("Delete brand with id: {}", brandId);
-    if (brandRepository.findByBrandId(Integer.parseInt(brandId)).isEmpty()) {
-      logger.error("Error deleting brand with id: {}", brandId);
-      throw new BrandNotFoundException("Brand not found!");
+    @Override
+    public BrandSearchResponseDto search(String searchQuery, Pageable page, BrandSearchFilterDto filter) {
+        logger.info("Start brand search with query: {} and filters: {}", searchQuery, filter);
+        return customBrandRepository.search(searchQuery, page, filter);
     }
-    brandRepository.deleteByBrandId(Integer.parseInt(brandId));
-    logger.info("Brand with id {} is deleted successfully.", brandId);
-  }
 
-  @Override
-  public List<Brand> getAllBrandsByCategoryId(String categoryId) {
-    logger.info("Get all brands with category id: {}", categoryId);
-    // Need to add category check
-    return brandRepository.findAllByCategoryId(Integer.parseInt(categoryId));
-  }
-
-  @Override
-  public BrandSearchResponseDto search(String searchQuery, Pageable page, BrandSearchFilterDto filter) {
-    logger.info("Start brand search with query: {} and filters: {}", searchQuery, filter);
-    return customBrandRepository.search(searchQuery, page, filter);
-  }
-
-  @Override
-  public void bulkCreateBrand(List<BrandDto> brandDtoList) {
-    logger.info("Starting bulkCreateBrand");
+    @Override
+    public void bulkCreateBrand(List<BrandDto> brandDtoList) {
+        logger.info("Starting bulkCreateBrand");
 //    for (BrandDto brandDto : brandDtoList) {
 //      CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(brandTopic, brandDto.getBrandId().toString(), brandDto);
 //      future.whenComplete((result, ex) -> {
@@ -142,5 +142,5 @@ public class BrandServiceImpl implements BrandService {
 //        }
 //      });
 //    }
-  }
+    }
 }
