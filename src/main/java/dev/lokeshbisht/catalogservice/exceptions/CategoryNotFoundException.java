ef9a440b9a1788a7d1ba1df81a6c568119ce1ab9
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-12-07 23:49:43 +0530

    Correct code formatting and add license

--- src/main/java/dev/lokeshbisht/catalogservice/exceptions/CategoryNotFoundException.java
+++ src/main/java/dev/lokeshbisht/catalogservice/exceptions/CategoryNotFoundException.java
@@ -1,10 +1,16 @@
+/**
+ * Copyright (C) 2023 Lokesh Bisht
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package dev.lokeshbisht.catalogservice.exceptions;
 
 public class CategoryNotFoundException extends RuntimeException {
 
-  private static final long serialVersionID = 1L;
+    private static final long serialVersionID = 1L;
 
-  public CategoryNotFoundException(String message) {
-    super(message);
-  }
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
 }
